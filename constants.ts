e14cf809a536be5bee07bd7ae75dfbe1ed74055a
MangousteEnFeu <161948048+MangousteEnFeu@users.noreply.github.com>
2025-12-18 18:08:57 +0100

    Change key api

--- constants.ts
+++ constants.ts
@@ -1,5 +1,3 @@
-export const GEMINI_API_KEY = "AIzaSyC5cs3_gulQ8wWtoDxD72gYr3KyjF-hNKw";
-
 export const SWISS_AMOUNT_PATTERN = /-?\d{1,3}(?:[']\d{3})*(?:\.\d{2})?/g;
 export const IBAN_PATTERN = /CH\d{2}[\s]?\d{4}[\s]?\d{4}[\s]?\d{4}[\s]?\d{4}[\s]?[\d\w]{0,3}/gi;
 export const DATE_PATTERN = /\d{1,2}\.\d{1,2}\.\d{4}/g;
